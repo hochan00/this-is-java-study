080078de779352cb82e7383dfc7718fe10b4599c
HoChanYu <yhc6563@gmail.com>
2026-04-23 20:52:58 +0900

    Chapter5

---
+++ Chapter05/src/EmptyStringExample.java
@@ -0,0 +1,8 @@
+public class EmptyStringExample {
+    public static void main(String[] args) {
+        String hobby = "";
+        if (hobby.equals("")) {
+            System.out.println("빈 문자열");
+        }
+    }
+}
